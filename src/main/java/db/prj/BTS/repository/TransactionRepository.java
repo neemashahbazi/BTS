287361f20a868176d28ae7675f78b675571f2f2e
Pooneh Mousavi <mousavi.pooneh@gmail.com>
2018-12-05 15:54:24 -0600

    fix get report bug

--- src/main/java/db/prj/BTS/repository/TransactionRepository.java
+++ src/main/java/db/prj/BTS/repository/TransactionRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
 
 
-    @Query(value = "Select sum(amount) from trx_transactions where  date= ?1", nativeQuery = true)
+    @Query(value = "Select count(id) from trx_transactions where  date= ?1", nativeQuery = true)
     public int  getDailyTransaction(Date date);
 
 
-    @Query(value = "Select sum(amount) from trx_transactions where  date between  ?1 and ?2", nativeQuery = true)
+    @Query(value = "Select count(id) from trx_transactions where  date between  ?1 and ?2", nativeQuery = true)
     public int getTransactionBetweenRange(Date begin, Date end);
 }
